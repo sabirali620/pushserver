fb4519ca38e35e2fe9909f918ed74458a5a8b8a5
sabirali620 <sabir@owlmgp.com>
2025-01-03 21:43:26 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('push', function (event) {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon,
+      data: data,
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  if (event.notification.data && event.notification.data.url) {
+    clients.openWindow(event.notification.data.url);
+  }
+});
